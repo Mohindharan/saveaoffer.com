23b55a8f2c6ae8ed991ac4ee9d3144791e8a1edc
Mohindharan <mohindharan13@gmail.com>
2015-12-16 13:11:39 +0530

    start

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Rust and diamonds</title>
+	<script src="bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="bower_components/angularjs/angular.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg">
+</head>
+<body>
+<nav>
+	<ul class="navigation">
+		<li class="department-menu"><a href="#">department <i class="glyphicon glyphicon-triangle-bottom"></i></a></li>
+		<li><a href="#"></a></li>	
+		<li><a href="#"></a></li>
+		<li><i class="glyphicon glyphicon-option-vertical"></i></li>
+	</ul>
+</nav>
+	
+</body>
+</html>
